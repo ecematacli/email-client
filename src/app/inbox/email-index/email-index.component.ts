d49337f91dd7a58d6c2b3f4bb040c600264650da
Ecem Atacli <ecematacli@gmail.com>
2020-05-29 20:27:04 +0200

    Add components to inbox module and add routing rules to render its children

---
+++ src/app/inbox/email-index/email-index.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EmailService } from '../email.service';
+
+@Component({
+  selector: 'app-email-index',
+  templateUrl: './email-index.component.html',
+  styleUrls: ['./email-index.component.css'],
+})
+export class EmailIndexComponent implements OnInit {
+  emails = [];
+
+  constructor(private emailService: EmailService) {
+    console.log('constructor runs!');
+  }
+
+  ngOnInit(): void {
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails;
+    });
+  }
+}
