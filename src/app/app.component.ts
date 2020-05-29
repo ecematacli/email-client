0844e7defef02b3905be2562ee3cf5df59241c53
Ecem Atacli <ecematacli@gmail.com>
2020-05-29 16:59:08 +0200

    Complete the auth service guard functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ export class AppComponent {
   constructor(private authService: AuthService) {
     this.signedin$ = this.authService.signedin$;
   }
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(() => {});
+  }
 }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Resolve,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, skipWhile, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.signedin$.pipe(
+      skipWhile((value) => value === null),
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
   }
 }
