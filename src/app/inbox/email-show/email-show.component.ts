9dca78e968d5dbd14b6809fdd642c760e9e93751
Ecem Atacli <ecematacli@gmail.com>
2020-05-29 20:45:52 +0200

    Add activated route with dependency injection to grab the url params

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  styleUrls: ['./email-show.component.css'],
 })
 export class EmailShowComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((snapshot) => {
+      console.log(snapshot);
+    });
   }
-
 }
