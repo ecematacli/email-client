538cb51457c67745dc7c809fd6e6485c9eb37eb6
Mert Tekmir <m.tekmir@newmotion.com>
2020-05-30 16:34:12 +0200

    Add modal reusable component and compose button

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-email-create',
   templateUrl: './email-create.component.html',
-  styleUrls: ['./email-create.component.css']
+  styleUrls: ['./email-create.component.css'],
 })
 export class EmailCreateComponent implements OnInit {
+  showModal = false
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { InputComponent } from './input/input.component'
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, ModalComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent],
+  exports: [InputComponent, ModalComponent],
 })
 export class SharedModule {}
